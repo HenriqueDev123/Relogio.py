3b4802e55884029f421b6bfd7452e9ca07c1ce40
HenriqueRas1 <141374440+HenriqueRas1@users.noreply.github.com>
2023-08-22 16:24:23 -0300

    Update relogio.py

--- relogio.py
+++ relogio.py
@@ -1,16 +1,39 @@
 import tkinter as tk
 import time
 
-
 class ClockApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Relógio")
+        self.root.bind("<KeyPress>", self.handle_key_press)
+        self.root.bind("<KeyRelease>", self.handle_key_release)
 
         self.time_label = tk.Label(root, font=("Helvetica", 200))
         self.time_label.pack()
 
+        self.font_size = 200
         self.update_time()
+
+    def handle_key_press(self, event):
+        if event.keysym == "equal":
+            self.increase_font_size()
+        elif event.keysym == "minus":
+            self.decrease_font_size() 
+
+    def handle_key_release(self, event):
+        pass
+
+    def increase_font_size(self):
+        self.font_size += 10
+        self.update_time_font()
+
+    def decrease_font_size(self):
+        self.font_size = max(50, self.font_size - 10)
+        self.update_time_font()
+
+    def update_time_font(self):
+        new_font = ("Helvetica", self.font_size)
+        self.time_label.config(font=new_font)
 
     def update_time(self):
         current_time = time.localtime()
